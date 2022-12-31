f69b1faa8f9704adb626e936165fac8297f88565
Jail_IB <jailsonsanches9@gmail.com>
2022-12-31 04:23:52 +0100

    feat: feed screen

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -2,18 +2,47 @@ import { useState, useEffect } from "@mui/material";
 
 import { Box, Stack, Typography } from "@mui/material";
 import React from "react";
-import Sidebar from "./Sidebar";
+import {Sidebar, Videos} from "../components";
+
 
 const Feed = () => {
   return (
     <Stack sx={{ flexDirection: { sx: "column", md: "row" } }}>
-      <Box sx={{ height: { sx: "auto", md: "92vh" }, borderRight: "10px solid #3d3d3d", px: { sx: 0, md: 2 } }}>
+      <Box
+        sx={{
+          height: { sx: "auto", md: "92vh" },
+          borderRight: "10px solid #3d3d3d",
+          px: { sx: 0, md: 2 },
+        }}
+      >
         <Sidebar />
-        <Typography variant="body2" sx={{mt:1.5,
-        color:'#fff'}}>
+        <Typography variant="body2" sx={{ mt: 1.5, color: "#fff" }}>
           Copyright 2022 JSM Media
         </Typography>
       </Box>
+      <Box p={2} sx={{ overflow: "auto", height: "9vh", flex: 2 }}>
+        <Typography
+          variant="h4"
+          fontWeight="bold"
+          mb={2}
+          ml={4}
+          sx={{
+            color: "#fff",
+          }}
+        >
+         
+          Jailson
+          {" "}
+          <span
+            style={{
+              color: "#F31503",
+            }}
+          >
+            videos
+          </span>
+        </Typography>
+      </Box>
+      <Videos />
     </Stack>
   );
 };
