768e03b1a9e432688b1e00b61571a54919fb491c
Jail_IB <jailsonsanches9@gmail.com>
2023-01-01 22:18:57 +0100

    feat:channel video card

--- src/components/ChannelDetail.jsx
+++ src/components/ChannelDetail.jsx
@@ -1,9 +1,48 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { Box } from "@mui/material";
+
+import ChannelVideo from './ChannelVideo'
+import Videos from './Videos'
+
+import { fetchAPI } from "../utils/fetchAPI";
 
 const ChannelDetail = () => {
-  return (
-    <div>ChannelDetail</div>
-  )
-}
+  const [channelDetail, setChannelDetail] = useState(null);
+  const [videos, setVideos] = useState([]);
+  const { id } = useParams();
 
-export default ChannelDetail
+  // console.log(channelDetail);
+  console.log(videos);
+
+  useEffect(() => {
+    fetchAPI(`channels?part=snippet&id=${id}`).then((data) =>
+      setChannelDetail(data?.items[0])
+    )
+    fetchAPI(`search?channelId=${id}&part=snippet&order=date`).then((data) =>
+      setVideos(data?.items)
+    )
+  }, [id]);
+
+  return (
+    <Box sx={{ minHeight: "95vh" }}>
+      <Box>
+        <div style={{
+          background: 'linear-gradient(90deg, rgba(141,126,149,1) 24%, rgba(176,102,201,1) 50%, rgba(141,126,149,1) 76%)',
+          zIndex:'10',
+          height:'200px'
+        }}/>
+          <ChannelVideo marginTop='-100px' channelDetail={channelDetail} />
+      </Box>
+      <Box display='flex' p={2} >
+        <Box sx={{mr:{sm:'130px'}}} />
+          <Videos videos={videos} />
+
+      </Box>
+
+      
+    </Box>
+    );
+};
+
+export default ChannelDetail;
